88b1eff487cf8fb2c99d1058c0142bc8390f85c7
djanerch <dchoban693@gmail.com>
2022-04-19 21:36:14 +0300

    SnakeGame

---
+++ StartUp.cs
@@ -0,0 +1,20 @@
+﻿using SimpleSnake.Utilities;
+using SimpleSnakeGame.GameObjects;
+using System;
+
+namespace SimpleSnakeGame
+{
+    class StartUp
+    {
+        public static void Main()
+        {
+            ConsoleWindow.CustomizeConsole();
+
+            Wall wall = new Wall(100, 40);
+            Snake snake = new Snake(wall);
+
+            Engine engine = new Engine(snake, wall);
+            engine.Run();
+        }
+    }
+}
